63fc829f34a738558317927651773b8024678b7e
s24593 <92479375+Shriksa@users.noreply.github.com>
2022-12-16 01:40:58 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,62 +1,87 @@
 import java.util.Scanner;
-import java.util.Random;
-
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("---===1===---");
-        System.out.println("Wprowadz dwie liczby calkowite: ");
-        String pierwszy_wiersz = keyboard.nextLine();
-        String drugi_wiersz = keyboard.nextLine();
-        int pierwsza_L = Integer.parseInt(pierwszy_wiersz);
-        int druga_L = Integer.parseInt(drugi_wiersz);
-        System.out.println("Wybierz operacje: ");
-        System.out.println("ADD: +");
-        System.out.println("SUB: -");
-        System.out.println("DIV: /");
-        System.out.println("MUL: *");
-        String Operacja = keyboard.nextLine();
-        int Wynik = 0;
-        switch (Operacja) {
-            case ("ADD"):
-                Wynik = pierwsza_L + druga_L;
-                break;
-            case ("SUB"):
-                Wynik = pierwsza_L - druga_L;
-                break;
-            case ("DIV"):
-                Wynik = pierwsza_L / druga_L;
-                break;
-            case ("MUL"):
-                Wynik = pierwsza_L * druga_L;
-                break;
+        System.out.println("Wpisz liczbe n: ");
+        int n = keyboard.nextInt();
+        System.out.println("<--==Zadanie 1==-->");
+        System.out.println(Fibanacci(n));
+        System.out.println("<--==Zadanie 2==-->");
+        System.out.println(Rekurencja(n));
+        System.out.println("<--==Zadanie 3==-->");
+        System.out.println("Wpisz liczbe w tablice: ");
+        int n1 = keyboard.nextInt();
+        int tablica[] = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            tablica[i] = keyboard.nextInt();
         }
-        System.out.println("Wynik: " + Wynik);
-        System.out.println("Wykorzystanie metody absoluteValue: ");
-        absoluteValue(Wynik);
-        System.out.println("<---===3===--->");
-        System.out.println("Generator losowych 5 znakow: ");
-        getRandomString(5);
+        System.out.println("Normalna kolejność: ");
+        for (int i = 0; i < n1; i++) {
+            System.out.print(tablica[i] + ", ");
+        }
+        System.out.println("Odwrotna kolejność: " + Odwrotna_tablica(tablica));
+        System.out.println("<--==Zadanie 4==-->");
+        System.out.println("Wpisz key: ");
+        int key = keyboard.nextInt();
+        System.out.println(Zadanie_4(tablica, key));
+        System.out.println(Zadanie_4_Rekurencja(tablica, tablica[0], tablica[tablica.length - 1], key));
+    }
 
+    public static int Fibanacci(int n){
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for(int i = 0; i < n; i++){
+            c = a;
+            a = b;
+            b = c + b;
+        }
+        return(a);
     }
-    public static void absoluteValue(int a) {
-        if (a < 0){
-            a = a - (2 * a);
-            System.out.println("Metoda zmienia liczbe na dodatnia: " + a);
+
+    public static int Rekurencja(int n){
+        if ( n == 0) {
+            return 0;
+        } else if (n == 1){
+            return 1;
         } else {
-            System.out.println("Liczba jest dodatnia, metoda nie zmienia wartosc: " + a);
+            return Rekurencja(n-1) + Rekurencja(n-2);
         }
     }
-    public static String getRandomString(int lenght){
-        String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        Random random_symbol = new Random();
-        StringBuffer sb =new StringBuffer();
-        for(int i = 0; i < lenght; i++){
-            int number = random_symbol.nextInt(62);
-            sb.append(str.charAt(number));
+
+    public static int Odwrotna_tablica(int tablica[]){
+        for (int i = tablica.length - 1; i >= 0; i--) {
+            System.out.print(tablica[i] + ", ");
         }
-        System.out.println(sb);
-        return sb.toString();
+        return 0;
+    }
+
+    public static int Zadanie_4(int tablica[], int key){
+        boolean Wynik = false;
+        while (!Wynik){
+            for (int i = 0; i <= tablica.length - 1; i++){
+                if(tablica[i] == key){
+                    System.out.println(tablica[i] + " <--- Key");
+                    Wynik = true;
+                    break;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static int Zadanie_4_Rekurencja(int tablica[], int pierwszy_Element, int ostatni_Element, int szukany_Element) {
+        if (ostatni_Element >= pierwszy_Element) {
+            int middle = (ostatni_Element + pierwszy_Element) / 2;
+            if (tablica[middle] == szukany_Element) {
+                return middle;
+            }
+            if (tablica[middle] > szukany_Element) {
+                return Zadanie_4_Rekurencja(tablica, pierwszy_Element, middle - 1, szukany_Element);
+            }
+                return Zadanie_4_Rekurencja(tablica, middle + 1, ostatni_Element, szukany_Element);
+        }
+        return -1;
     }
 }
